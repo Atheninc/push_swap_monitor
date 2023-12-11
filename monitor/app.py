e581f85f2ffaeb4d5d7d47be9db09e68fbd07f25
Atheninc <adri12tena@gmail.com>
2023-12-11 10:33:57 +0100

    Add files via upload

--- monitor/app.py
+++ monitor/app.py
@@ -4,6 +4,19 @@ import os
 eel.init('web')
 
 @eel.expose
+def ExecCmd(cmd):
+    print("exec : " + str(cmd))
+    try:
+        rep = os.popen(cmd).read()
+    except Exception as e:
+        rep = "Error: " + str(e)
+    except OSError as e:
+        rep = "Error: " + str(e)
+
+    print(rep)
+    return(rep)
+
+@eel.expose
 def GetData(val, path):
     tmps = []
     txt = ""
@@ -18,6 +31,7 @@ def GetData(val, path):
     if (os.path.isfile(path) == False):
         return "path Error"
     try:
+        print("exec : " + str("./" + path + " " + txt))
         rep = os.popen("./" + path + " " + txt).read()
     except:
         rep = "path Error"
